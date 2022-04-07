bbe2a815c82b5cfe4cbf4865f206d3c02af2f0b4
Daniil Zhuravlev <nakrovati@outlook.com>
2022-04-08 02:37:49 +0300

    feat: add title add subtitle for application

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <hr class="line" />
   <TheHeader />
-  <main class="inner">
-    <div class="container"></div>
+  <main class="intro">
+    <div class="container">
+      <div class="inner">
+        <h1 class="inner__title">Shorten the link</h1>
+        <h2 class="inner__subtitle">
+          Create a short link to copy to your website, social media, and
+          messengers.
+        </h2>
+        <img src="" alt="" class="inner__image" />
+      </div>
+      <div class="form-of-reduction"></div>
+    </div>
   </main>
   <TheFooter />
 </template>
@@ -27,5 +37,34 @@ export default {
   background-color: color.scale($color: colors.$primary, $lightness: 25%);
   height: 6px;
   margin: 0;
+}
+
+.intro {
+  padding: 0 20px;
+
+  .container {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 100px 0;
+    width: 100%;
+  }
+}
+
+.inner {
+  font-family: Lekton, Arial, Helvetica, sans-serif;
+  padding: 20px;
+  
+  &__title {
+    font-size: 64px;
+    font-weight: bold;
+    margin: 0;
+  }
+  &__subtitle {
+    font-size: 24px;
+  }
+  &__image {
+    height: auto;
+    max-width: 100%;
+  }
 }
 </style>
