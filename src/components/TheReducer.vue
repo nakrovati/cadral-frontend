27d5788a4dff7a235bbc320d3f233b402e06f705
Daniil Zhuravlev <nakrovati@outlook.com>
2022-05-09 01:49:21 +0300

    refactor: add an example url to reducer.vue

--- src/components/TheReducer.vue
+++ src/components/TheReducer.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <TheReducerInput />
+    <div class="url-example">Example: https://google.com</div>
   </div>
 </template>
 
-<script>
+<script setup>
 import TheReducerInput from "Components/TheReducerInput.vue";
-
-export default {
-  name: "Reducer",
-  components: { TheReducerInput },
-};
 </script>
+
+<style lang="scss" scoped>
+.url-example {
+  font-size: 0.75em;
+  margin: 10px 20px 30px;
+}
+</style>
