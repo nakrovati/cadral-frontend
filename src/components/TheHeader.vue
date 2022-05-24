ddb59efb19f7802761666e65db884c92cd01f53b
Daniil Zhuravlev <nakrovati@outlook.com>
2022-05-24 22:08:24 +0300

    style: add a transition on hovering over the url in the header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="header">
-    <a href="/" class="header__logo">Rapid URL</a>
+    <div class="logo">
+      <a href="/">Rapid URL</a>
+    </div>
     <nav class="menu">
       <ul class="menu__list">
         <li class="menu__item">
-          <a href="/origin-url/" class="nav-menu__item">show origin url</a>
+          <a href="/origin-url/">show origin url</a>
         </li>
       </ul>
     </nav>
@@ -20,19 +22,26 @@
   margin: 0 auto;
   max-width: 2560px;
   padding: 20px 40px;
-
-  .header__logo {
-    font-family: Lekton, Arial, Helvetica, sans-serif;
-    font-size: 1.5em;
-    font-weight: bold;
-  }
 }
 
-.menu__list {
-  display: flex;
+.logo {
+  font-family: Lekton, Arial, Helvetica, sans-serif;
+  font-size: 1.5em;
+  font-weight: bold;
+}
 
-  .menu__item {
-    list-style-type: none;
+.menu {
+  .menu__list {
+    display: flex;
+
+    .menu__item {
+      list-style-type: none;
+
+      &:hover {
+        color: colors.$primary;
+        transition: color 0.3s ease;
+      }
+    }
   }
 }
 
