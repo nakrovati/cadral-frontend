6f39db05f3b47815584ac22f5cb7c04485f3faeb
Daniil Zhuravlev <nakrovati@outlook.com>
2022-06-14 16:01:44 +0300

    refactor: 404 page for vue router

--- src/404/404.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import page404 from "./404.vue";
-import "@/styles/style.scss";
-
-createApp(page404).mount("#notfound");
---
+++ src/views/NotFound.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="intro">
+    <div class="container">
+      <img
+        alt="A robot with a magnifying glass searches for URLs"
+        ref="image"
+      />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import imageRobot404 from "Images/404-robot.jpg";
+import imageRobot404Safari from "Images/404-robot-safari.jpg";
+
+const image = ref();
+
+onMounted(() => {
+  const { userAgent } = navigator;
+
+  if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
+    image.value.src = imageRobot404Safari;
+  } else {
+    image.value.src = imageRobot404;
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.intro {
+  padding: 0 20px;
+
+  .container {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 100px 0;
+    width: 100%;
+  }
+}
+</style>
