3ea6d15736fe28497765f6eab7ff3aecdd362dec
Daniil Zhuravlev <nakrovati@outlook.com>
2022-06-14 14:48:57 +0300

    fix: i18n lang codes for html

--- src/configs/i18n.js
+++ src/configs/i18n.js
@@ -2,7 +2,7 @@ import { createI18n } from "vue-i18n";
 import messages from "@intlify/vite-plugin-vue-i18n/messages";
 
 const i18n = createI18n({
-  fallbackLocale: "en",
+  fallbackLocale: "en-US",
   messages,
 });
 
