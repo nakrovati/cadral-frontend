1b829f7b2129aa60fa9d84d379005c4401a6826b
Daniil Zhuravlev <nakrovati@outlook.com>
2022-06-20 00:02:16 +0300

    refactor: move OriginalUrl.vue file to views

--- src/origin-url/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./OriginUrl.vue";
-import "Styles/style.scss";
-
-createApp(App).mount("#app");
---
+++ src/views/OriginUrl.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="intro">
+    <div>{{ props.dateCreated }}</div>
+    <h3>{{ props.initialUrl }}</h3>
+    <p>{{ props.shortUrl }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  shortUrl: { type: String, required: true },
+  initialUrl: { type: String, required: true },
+  dateCreated: { type: String, required: true },
+});
+</script>
