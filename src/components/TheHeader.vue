3216826f3829103803927624d04d8e0a719880e8
Daniil Zhuravlev <nakrovati@outlook.com>
2022-07-08 00:52:23 +0300

    feat, refactor: make the header prettier

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,25 +1,25 @@
 <template>
   <header class="header">
-    <div class="logo">
-      <router-link to="/">Cadral URL</router-link>
+    <router-link class="header__logo" to="/">Cadral URL</router-link>
+    <div class="menu">
+      <nav class="menu__navigation">
+        <a @click="isOpen = true" class="navigation__item">
+          {{ t("header.showOriginUrl") }}
+        </a>
+        <teleport to="body">
+          <div class="modal" v-if="isOpen">
+            <TheOriginUrlModal @close="isOpen = false" />
+          </div>
+        </teleport>
+      </nav>
     </div>
-    <nav class="menu">
-      <button @click="isOpen = true">
-        {{ t("header.showOriginUrl") }}
-      </button>
-      <teleport to="body">
-        <div class="modal" v-if="isOpen">
-          <TheOriginUrlModal @close="isOpen = false" />
-        </div>
-      </teleport>
-    </nav>
   </header>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import { useI18n } from "vue-i18n";
-import TheOriginUrlModal from "Components/TheOriginUrlModal.vue";
+import TheOriginUrlModal from "./TheOriginUrlModal.vue";
 
 const { t } = useI18n();
 
@@ -27,9 +27,43 @@ const isOpen = ref(false);
 </script>
 
 <style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 2560px;
+  padding: 10px 40px;
+
+  .header__logo {
+    font-family: Lekton, Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+}
+
+.menu {
+  align-items: center;
+  display: flex;
+
+  .language-switcher {
+    height: 1.5em;
+  }
+
+  .menu__navigation {
+    background-color: inherit;
+    color: inherit;
+    white-space: nowrap;
+
+    &:hover {
+      color: colors.$primary;
+    }
+  }
+}
+
 .modal {
   align-items: center;
-  background-color: rgb(0 0 0 / 10%);
+  backdrop-filter: blur(0.5em);
+  background-color: rgb(0 0 0 / 40%);
   display: flex;
   height: 100%;
   justify-content: center;
@@ -40,43 +74,19 @@ const isOpen = ref(false);
   z-index: 999;
 
   > div {
-    background-color: #fff;
+    background-color: colors.$background-light;
     border-radius: 10px;
-    padding: 50px;
-  }
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  max-width: 2560px;
-  padding: 10px 40px;
-}
-
-.logo {
-  font-family: Lekton, Arial, Helvetica, sans-serif;
-  font-size: 1.5em;
-  font-weight: bold;
-}
-
-.menu {
-  display: flex;
-
-  button {
-    background-color: inherit;
-    color: inherit;
-    padding: 0;
-
-    &:hover {
-      color: colors.$primary;
-    }
+    padding: 40px;
   }
 }
 
 @media (prefers-color-scheme: dark) {
   .header {
     color: colors.$text-white;
+  }
+
+  .modal > div {
+    background-color: colors.$background-dark;
   }
 }
 </style>
