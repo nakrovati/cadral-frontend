0eeeff462b83ca49257a01ff8a618200559032cc
Daniil Zhuravlev <nakrovati@outlook.com>
2022-07-10 20:24:19 +0300

    chore: remove unused code from NotFoundPage

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -9,23 +9,7 @@
   </main>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
-import imageRobot404 from "Images/404-robot.jpg";
-import imageRobot404Safari from "Images/404-robot-safari.jpg";
-
-const image = ref();
-
-onMounted(() => {
-  const { userAgent } = navigator;
-
-  if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
-    image.value.src = imageRobot404Safari;
-  } else {
-    image.value.src = imageRobot404;
-  }
-});
-</script>
+<script setup></script>
 
 <style lang="scss" scoped>
 .intro {
