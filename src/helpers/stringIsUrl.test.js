d521a4780f5f4fdac24d2b6a6c447be35a98078b
Daniil Zhuravlev <nakrovati@outlook.com>
2022-07-11 20:01:36 +0300

    test: add falsy use cases to stringIsUrl fun

--- src/helpers/stringIsUrl.test.js
+++ src/helpers/stringIsUrl.test.js
@@ -2,43 +2,66 @@ import { describe, expect, it } from "vitest";
 import isStringUrl from "./stringIsUrl";
 
 describe("RegExp: URI", function () {
+  /*
+    Valid values
+  */
+
   it("matches: google.com", () => {
     const url = "google.com";
-    expect(isStringUrl(url)).toBe(true);
+    expect(isStringUrl(url)).toBeTruthy();
   });
 
   it("matches: google.com/search?q=jest", () => {
     const url = "google.com/search?q=jest";
-    expect(isStringUrl(url)).toBe(true);
+    expect(isStringUrl(url)).toBeTruthy();
   });
 
   it("matches: https://google.com", () => {
     const url = "https://google.com";
-    expect(isStringUrl(url)).toBe(true);
+    expect(isStringUrl(url)).toBeTruthy();
+  });
+
+  it("matches: https://google.com:3000", () => {
+    const url = "https://google.com:3000";
+    expect(isStringUrl(url)).toBeTruthy();
   });
 
   it("matches: https://www.google.com/search?q=jest", () => {
     const url = "https://www.google.com/search?q=jest";
-    expect(isStringUrl(url)).toBe(true);
+    expect(isStringUrl(url)).toBeTruthy();
+  });
+
+  /*
+    Unvalid values
+  */
+
+  it("matches: ''", () => {
+    const url = "";
+    expect(isStringUrl(url)).toBeFalsy();
+  });
+
+  it("matches: 1 (number)", () => {
+    const url = "1";
+    expect(isStringUrl(url)).toBeFalsy();
   });
 
   it("doesn't match: https:/google.com", () => {
     const url = "https:/google.com";
-    expect(isStringUrl(url)).toBe(false);
+    expect(isStringUrl(url)).toBeFalsy();
   });
 
   it("doesn't match: https//google.com", () => {
     const url = "https//google.com";
-    expect(isStringUrl(url)).toBe(false);
+    expect(isStringUrl(url)).toBeFalsy();
   });
 
   it("doesn't match: asdadaad", () => {
     const url = "asdadaad";
-    expect(isStringUrl(url)).toBe(false);
+    expect(isStringUrl(url)).toBeFalsy();
   });
 
   it("doesn't match: tel:+78881234567", () => {
     const url = "tel:+78881234567";
-    expect(isStringUrl(url)).toBe(false);
+    expect(isStringUrl(url)).toBeFalsy();
   });
 });
