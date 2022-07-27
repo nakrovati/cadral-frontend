0be71289cc4c0868493366b1e52650f6cb8cb4c6
Daniil Zhuravlev <nakrovati@outlook.com>
2022-07-27 17:36:24 +0300

    fix: vue-i18n browser warning

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,9 @@ export default defineConfig({
     },
   },
   define: {
+    __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
   },
   resolve: {
     alias: {
